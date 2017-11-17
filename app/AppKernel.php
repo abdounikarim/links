7049c13822b8f0751f41a0e2ad93a9a157fc53e6
casrime <casrime@macbook-pro-de-casrime.home>
2017-11-17 16:38:38 +0100

    removing tinymce bundle, using only tinymce with cdn

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
-            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new AppBundle\AppBundle(),
         ];
 
